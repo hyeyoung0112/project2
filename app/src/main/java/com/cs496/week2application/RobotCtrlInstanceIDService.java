c46f6ea8b9c5b975bd7922d0164f6860bf4f1829
KimSinjeong <2725264@gmail.com>
2019-01-08 14:24:03 +0900

    CCTY is implemented

---
+++ app/src/main/java/com/cs496/week2application/RobotCtrlInstanceIDService.java
@@ -0,0 +1,53 @@
+package com.cs496.week2application;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+public class RobotCtrlInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = new FormBody.Builder()
+                .add("Token", token)
+                .build();
+        Log.d(TAG,"Body : " +body);
+
+        Request request = new Request.Builder()
+                .url("https://madcampproject2.firebaseio.com")
+                .post(body)
+                .build();
+
+        try {
+            client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- app/src/main/java/com/cs496/week2application/Tab3CCTV.java
+++ app/src/main/java/com/cs496/week2application/Tab3CCTV.java
@@ -1,17 +1,32 @@
 package com.cs496.week2application;
 
+import android.util.Log;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 public class Tab3CCTV extends Fragment {
+    String STREAMING_SERVER_URL = "http://192.168.43.139:8081/";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View rootView = inflater.inflate(R.layout.tab3cctv, container, false);
+        WebView webView = (WebView) rootView.findViewById(R.id.cctvWebView);
+        webView.setPadding(0,0,0,0);
+        //webView.setInitialScale(100);
+        webView.getSettings().setBuiltInZoomControls(false);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        //webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+
+        String url =STREAMING_SERVER_URL;
+        webView.loadUrl(url);
+
         return rootView;
     }
 }
